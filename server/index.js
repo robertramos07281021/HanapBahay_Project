120a91606f26e4781fdc024a703733aab8cdc298
robert dela cruz ramos <126318863+robertramos07281021@users.noreply.github.com>
2026-01-19 19:27:37 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
+import "dotenv/config.js";
 import { rentalRouter } from "./routes/rentalsRouter.js";
 import { userRoutes } from "./routes/userRouter.js";
 import { fileURLToPath } from "url";
@@ -11,7 +11,7 @@ import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config();
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
